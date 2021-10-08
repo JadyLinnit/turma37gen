8085ab480de3c243414f0d6fa9de64999b875e58
JadyLinnit <linnitjady@gmail.com>
2021-10-08 20:50:44 -0300

    PolimorfismoeHerança

---
+++ java/Lista6Atv1/src/entities/Preguica.java
@@ -0,0 +1,21 @@
+
+package entities;
+
+	public class Preguica extends Animal {
+	
+public Preguica(String nome, int idade) {
+	super(nome, idade);
+	
+}
+
+@Override
+public String habilidade() {
+	return "SOBE EM ARVORES!!!";
+}
+
+@Override
+public String som() {
+	return "zzzzzzzzz";
+}
+
+}
