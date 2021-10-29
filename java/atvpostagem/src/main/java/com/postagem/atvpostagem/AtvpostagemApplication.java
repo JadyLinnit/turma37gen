7989afa85589e5802041b100e6c561b22a9820c5
JadyLinnit <linnitjady@gmail.com>
2021-10-29 00:05:34 -0300

    Atv Postagem em Spring Boot

---
+++ java/atvpostagem/src/main/java/com/postagem/atvpostagem/AtvpostagemApplication.java
@@ -0,0 +1,13 @@
+package com.postagem.atvpostagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvpostagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvpostagemApplication.class, args);
+	}
+
+}
---
+++ java/atvpostagem/src/test/java/com/postagem/atvpostagem/AtvpostagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postagem.atvpostagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtvpostagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
