c57c90862b46527e8b6f387800fd1a561f3fb365
JadyLinnit <linnitjady@gmail.com>
2021-10-29 23:26:02 -0300

    Atv Postagem em Spring Boot

---
+++ MySQL/ListaSpringBoot/postagem.sql
@@ -0,0 +1,5 @@
+CREATE database nomeDoBanco;
+ USE blog_pessoal;
+ 
+ INSERT INTO postagem(`date`, texto, titulo)
+VALUES ("2020-04-03 01:04:25.000" , "MEU NOME É JADY", "SPRING BOOT");
--- java/atvpostagem/src/main/java/com/postagem/atvpostagem/repository/PostagemRepository.java
+++ java/atvpostagem/src/main/java/com/postagem/atvpostagem/repository/PostagemRepository.java
@@ -8,6 +8,9 @@ import com.postagem.atvpostagem.model.Postagem;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
-	public List<Postagem> findAllByTituloContaingIgnoreCase (String titulo);
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+	
+
+	public Postagem getByTituloContainingIgnoreCase(String titulo);
 
 }
