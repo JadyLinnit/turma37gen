e25592fbb1885b69ce839737246c093257cd8076
JadyLinnit <linnitjady@gmail.com>
2021-11-03 21:18:44 -0300

    Atv Postagem em Spring Boot

---
+++ java/atvpostagem/src/main/java/com/postagem/atvpostagem/repository/TemaRepository.java
@@ -0,0 +1,13 @@
+package com.postagem.atvpostagem.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.postagem.atvpostagem.model.Tema;
+
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+	
+}
